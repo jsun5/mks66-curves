983bd59480e43820881b5db64980ec3b4d1a17eb
jsun5 <jsun5@stuy.edu>
2019-03-11 00:08:43 -0400

    all done

--- main.py
+++ main.py
@@ -20,3 +20,4 @@ transform = new_matrix()
 # print_matrix( make_rotZ(math.pi/4) )
 
 parse_file( 'script', edges, transform, screen, color )
+#parse_file( 'huh.txt', edges, transform, screen, color )
